7a2540627dee4b2911c8311ea96aec069f21b7a6
Imanda <asis03ktm@gmail.com>
2022-01-23 10:05:51 +0545

    1

---
+++ html file/js file/about_function.js
@@ -0,0 +1,17 @@
+//funtion declaration
+//we can call before and after making function
+
+function greeting(){
+    console.log('GoodMorning!');
+}
+
+greeting();
+
+
+//function expression
+//We cannot call before making function
+
+const speak = function(){
+    console.log('I am speaking');
+}
+speak();
