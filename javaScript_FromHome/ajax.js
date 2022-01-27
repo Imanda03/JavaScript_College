6d0298eac6c330b4f1612265c14d68402ae88074
Imanda <asis03ktm@gmail.com>
2022-01-27 21:39:02 +0545

    AJAX

--- javaScript_FromHome/ajax.js
+++ javaScript_FromHome/ajax.js
@@ -13,5 +13,18 @@ function buttonClickHandel(){
     const xhr = new XMLHttpRequest();
 
     //open the object
-    xhr.open('GET','anish.txt',true)
+    xhr.open('GET','anish.txt',true);
+
+    //What to do in progress
+    xhr.onprogress = function(){
+        console.log('On progress');
+    }
+
+    //What to do when progress is ready
+    xhr.onload = function (){
+        console.log(this.responseText);
+    }
+
+    //Send the request
+    xhr.send();
 }
