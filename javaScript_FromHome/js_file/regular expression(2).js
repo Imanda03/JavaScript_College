464ac81e0e930f702741f8a4d77df9b779378758
Imanda <asis03ktm@gmail.com>
2022-02-06 21:47:00 +0545

    MetaCharacters

--- javaScript_FromHome/js_file/regular expression(2).js
+++ javaScript_FromHome/js_file/regular expression(2).js
@@ -10,6 +10,7 @@ regX = /[d$]/               // $ --> This mean sring will match if it has in the
 regX = /[an.ish]/             // . --> This mean there can be one character between an and ish if there is it will match
 regX = /[an*ish]/           // * --> This will match 0 or more than 1 characters
 regX = /[an?is?h]/           // ? --> Question masrk after character mean it is optional.
+regX = /ani\*sh/            // \ --> Symbol after the \ is will be characters
 
 let str = "There is boy name anish who is working very hard";
 
