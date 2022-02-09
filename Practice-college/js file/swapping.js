77dc507f4d73fcd19d42cd76e47131495c5dd2f1
Imanda <asis03ktm@gmail.com>
2022-02-09 21:05:35 +0545

    change

--- Practice-college/js file/swapping.js
+++ Practice-college/js file/swapping.js
@@ -5,6 +5,7 @@ document.write('The second number before swap:'+ num2 +'</br>');
 num1 = num1+num2;
 num2 = num1-num2;
 num1 = num1-num2;
+
 document.write('The value of first number after swap is '+num1 + '<br/>');
 document.write('The value of second number after swap is '+ num2 + '<br/>');
 console.log(num2);
