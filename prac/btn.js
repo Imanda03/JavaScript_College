3ac81ecf395ecdb90e0ec278ac3fa41e80a19fb6
Imanda03 <asis03ktm@gmail.com>
2022-02-20 10:46:07 +0545

    new through html doc

---
+++ prac/btn.js
@@ -0,0 +1,32 @@
+// let btn = document.getElementById('btn');
+
+// btn.addEventListener('click',()=>{
+    //     alert('You have been click message');
+    // })
+    
+    // Incresing the number on click
+    // let displayElem = document.querySelector('#increase');
+    // let incrElem = document.querySelector('#btn');
+    // let count = 0;
+
+    // updateDisplay();
+
+    // incrElem.addEventListener('click',()=>{
+    //     count++;
+    //     updateDisplay();
+    // });
+
+    // function updateDisplay(){
+    //     displayElem.innerHTML = count;
+    // }
+
+    // let reset = document.querySelector('#reset');
+
+    // reset.addEventListener('click',()=>{
+    //     count = 0;
+    //     updateDisplay();
+    // })
+
+    let num = document.querySelector('#increase');
+    let btn = document.querySelector('#btn');
+    let btn2 = document.querySelector('#reset');
