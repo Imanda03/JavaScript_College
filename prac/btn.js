2eeda5c369f7b0b660a54ab4749e286497b21478
Imanda <asis03ktm@gmail.com>
2022-02-22 21:06:46 +0545

    corrected

---
+++ prac/btn.js
@@ -0,0 +1,35 @@
+// // let btn = document.getElementById('btn');
+
+// // btn.addEventListener('click',()=>{
+//     //     alert('You have been click message');
+//     // })
+    
+    // Incresing the number on click
+    let displayElem = document.querySelector('#increase');
+    let incrElem = document.querySelector('#btn');
+    let decrElem = document.querySelector('#btn2');
+    let count = 0;
+
+    updateDisplay();
+
+    incrElem.addEventListener('click',()=>{
+        count++;
+        updateDisplay();
+    });
+
+    decrElem.addEventListener('click',()=>{
+        count--;
+        updateDisplay();
+    });
+
+    function updateDisplay(){
+        displayElem.innerHTML = count;
+    }
+
+    let reset = document.querySelector('#reset');
+
+    reset.addEventListener('click',()=>{
+        count = 0;
+        updateDisplay();
+    })
+
---
+++ prac/btnExercise.js
@@ -0,0 +1,28 @@
+let buttons = document.querySelectorAll('button');
+let value = document.getElementById("value");
+let count = 0;
+
+function increase(){
+    count++;
+    display();
+}
+function decrease(){
+    count--;
+    display();
+}
+function reset(){
+    count = 0;
+    display();
+}
+function display(){
+    value.innerHTML=count;
+}
+
+
+
+buttons.forEach(function (btn) {
+    btn.addEventListener('click',function(e){
+        const id = e.currentTarget.attributes.id.value;
+        id === "btn"?increase(): id === "btn2"?decrease():reset();
+    })
+})
