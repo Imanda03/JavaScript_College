bcb6ea2b8dc0ffa43d960e8b0208a17442fb1286
Imanda <asis03ktm@gmail.com>
2022-02-22 21:55:08 +0545

    used ifElse condition

--- prac/todo.js
+++ prac/todo.js
@@ -4,14 +4,20 @@ const form = document.getElementById('form');
 const text = document.getElementById('input');
 const list = document.querySelector('.list');
 
-form.addEventListener('submit',(e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     createIteam(text.value);
-} )
+})
 
-function createIteam(item){
-    let html = `<li class="list-item">${item}<button class="btn delete">Delete</button></li>`
-    list.insertAdjacentHTML('beforeend',html);
-    text.value = "";
-    text.focus();
+function createIteam(item) {
+    if (text.value.length > 0) {
+        let html = `<li class="list-item">${item}<button class="btn delete">Delete</button></li>`
+        list.insertAdjacentHTML('beforeend', html);
+        text.value = "";
+        text.focus();
+
+    } else {
+        alert("error has been occured");
+
+    }
 }
