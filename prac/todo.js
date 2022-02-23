7c3100dfa632626127976917603c820845e43166
Imanda03 <asis03ktm@gmail.com>
2022-02-23 10:34:14 +0545

    delete function from clz

--- prac/todo.js
+++ prac/todo.js
@@ -6,18 +6,22 @@ const list = document.querySelector('.list');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    createIteam(text.value);
+    let val = text.value.trim();
+    if(val.length){
+        createIteam(text.value)
+    }
 })
 
 function createIteam(item) {
-    if (text.value.length > 0) {
-        let html = `<li class="list-item">${item}<button class="btn delete">Delete</button></li>`
+        let html = `<li class="list-item">${item}<button  class="btn delete">Delete</button></li>`
         list.insertAdjacentHTML('beforeend', html);
         text.value = "";
         text.focus();
 
-    } else {
-        alert("error has been occured");
-
-    }
 }
+
+list.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+})
