fe7d2fc2b29570909bf5acf354d20642d12c5515
Imanda03 <asis03ktm@gmail.com>
2022-03-14 10:26:07 +0545

    hideAndShow through css and js

---
+++ prac/hide and close css/hide.js
@@ -0,0 +1,10 @@
+console.log("Welcome");
+
+let btn = document.getElementById('hideBtn');
+
+btn.addEventListener('click',() =>{
+    var par = document.getElementById("hide");
+    par.classList.toggle('hide')
+})
+
+// toggle la on rw off garxa     yesma hide rw show garxa button ma click garda
