d53277782de0e2c526a4042db31b031912cdc2d2
Imanda03 <asis03ktm@gmail.com>
2022-03-22 10:34:49 +0545

    corrected

--- prac/emailValidation/emailV.js
+++ prac/emailValidation/emailV.js
@@ -1,19 +1,19 @@
 const email = document.querySelector('.email');
 const form = document.querySelector('.form');
 const feedback = document.querySelector('.para')
-let em = email.value;
-
-let atIndex = em.indexOf('@');
-let dotIndex = em.lastIndexOf('.');
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
+    let em = email.value;
+    
+    let atIndex = em.indexOf('@');
+    let dotIndex = em.lastIndexOf('.');
 
     if(atIndex < 1 || dotIndex >= em.length - 2 || dotIndex - atIndex < 3){
-        feedback.textContent = 'Email is valid'
+        feedback.textContent = 'Email is invalid'
     }
     else{
-        feedback.textContent = 'Email is not valid'
+        feedback.textContent = 'Email is valid'
     }
     
 })
